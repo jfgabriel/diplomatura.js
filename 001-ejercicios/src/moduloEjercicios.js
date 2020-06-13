b9b8ca2f79eb6e844d925bf3fb61f97a76215fd5
pdavid777 <66015887+pdavid777@users.noreply.github.com>
2020-06-13 00:03:30 -0300

    Reagregado Ej001
    Se había reestablecido cuando lo cloné...

--- 001-ejercicios/src/moduloEjercicios.js
+++ 001-ejercicios/src/moduloEjercicios.js
@@ -19,12 +19,56 @@ import basededatos from './basededatos';
       universidad: 2,
     }
   ]
- * @param {nombreAlumno} nombreAlumno
+ * @param {String} nombreAlumno el id del alumno
  */
+
+function getMateriasAprobadas(calificaciones) {
+  const materias=basededatos.materias;
+  let materiasCursadas=[];
+
+  for (let i=0; i<calificaciones.length; i++) {
+    let calificacion=calificaciones[i];
+    
+    for (let j=0; j<materias.length; j++) {
+      let materia=materias[j];
+
+      if (materia.id===calificacion.materia)
+        materiasCursadas.push(materia);
+    }
+  }
+  return materiasCursadas;
+}
+
+function wasApproved(calificacion) {
+  return calificacion>=4;
+}
+
+function getCalificaciones(alumnoId){
+  const calificaciones=basededatos.calificaciones;
+  let materiasAprobadas=[];
+  for (let i=0; i<calificaciones.length; i++) {
+    if (calificaciones[i].alumno===alumnoId) {
+      if (wasApproved(calificaciones[i].nota))
+        materiasAprobadas.push(calificaciones[i]);
+      }
+  }
+  return materiasAprobadas;
+}
+
 export const materiasAprobadasByNombreAlumno = (nombreAlumno) => {
-  // Ejemplo de como accedo a datos dentro de la base de datos
-  // console.log(basededatos.alumnos);
-  return [];
+  const alumnos=basededatos.alumnos;
+  let alumno;
+
+  for (let i = 0; i<alumnos.length; i++) {
+    if (alumnos[i].nombre.indexOf(nombreAlumno)>-1) {
+      alumno=alumnos[i];
+      break;
+    }
+  }
+  if (!alumno) return undefined;
+
+  const calificaciones=getCalificaciones(alumno.id);
+  return getMateriasAprobadas(calificaciones);
 };
 
 /**
@@ -68,9 +112,189 @@ export const materiasAprobadasByNombreAlumno = (nombreAlumno) => {
     }
  * @param {string} nombreUniversidad
  */
+
+/*function innerJoin(elementos,idsArray){
+  const arreglo=[];
+
+  elementos.forEach((elemento)=>{
+    if (idsArray.indexOf(elemento.id)>-1)
+      arreglo.push(elemento);
+  })
+  return arreglo;
+}
+
 export const expandirInfoUniversidadByNombre = (nombreUniversidad) => {
-  return {};
+  const universidades=basededatos.universidades;
+  let universidad,
+      infoUniversidad={};
+  for (let i=0; i<universidades.length; i++) {
+    if (universidades[i].nombre.indexOf(nombreUniversidad)>-1) {
+      universidad=universidades[i];
+      break;
+    }
+  }
+  if (!universidad) 
+    return undefined;
+  else infoUniversidad=universidad;
+
+  //Cargar las  materias según el Id de la universidad
+  infoUniversidad.materias=[];
+  for (let materia in basededatos.materias) {
+    if (basededatos.materias[materia].universidad===universidad.id)
+      infoUniversidad.materias.push(basededatos.materias[materia]);
+  }
+  
+  //------ Profesores -------
+  //
+  //Obtener los id de los profesores en base a todas las materias ya obtenidas
+  let profesoresId=[];
+  for (let materia in infoUniversidad.materias) {
+    let materiaProfesoresId=infoUniversidad.materias[materia].profesores;
+
+    for (let i=0; i<materiaProfesoresId.length; i++) {
+      let idActual=materiaProfesoresId[i];
+      //Filtrar para evitar que se agregue más de una vez cada profesor
+      if (profesoresId.indexOf(idActual)===-1)
+        profesoresId.push(idActual);
+    }
+  }  
+  //"Unir" las tabla profesores con los ids de profesores
+  infoUniversidad.profesores=innerJoin(basededatos.profesores,profesoresId);
+  
+  //------ Alumnos -------
+  //Trae las calificaciones donde estén las materias obtenidas, luego los alumnos calificados
+  //
+  //Obtener los id de los las materias en base a 
+  const materiasId = infoUniversidad.materias.map(materia=>materia.id);
+
+  const alumnosId=[];
+  for (var i in basededatos.calificaciones) {
+    let calificacion = basededatos.calificaciones[i];
+
+    if (materiasId.indexOf(calificacion.materia)>-1 && alumnosId.indexOf(calificacion.alumno)===-1) {
+      alumnosId.push(calificacion.alumno);
+    }
+  }
+
+  infoUniversidad.alumnos=innerJoin(basededatos.alumnos,alumnosId);
+  
+  return infoUniversidad;
+};*/
+
+
+///////////////////
+
+// Con operador urinario y otras funciones
+
+function innerJoin(elementos,idsArray){
+  return elementos.filter(elemento=>idsArray.includes(elemento.id));
+}
+
+export const expandirInfoUniversidadByNombre = (nombreUniversidad) => {
+  // Decorador urinario 
+  const urinary = fn => {
+    return (...args) => fn(args[0]);
+  }
+
+  const indexOf=(elemento)=>(needle)=>{
+    console.log('IndexOf---------')
+    console.log(elemento)
+    console.log(needle)
+    console.log('EndIndexOf---------')
+    return elemento.indexOf(needle);
+  }
+  const obtenerValor=(elemento)=>(valor)=>(patron)=>{
+    console.log('Obtener valor---------')
+    console.log(elemento)
+    console.log(valor)
+    console.log(elemento[valor])
+    console.log(patron)
+    console.log('EndObtener valor---------')
+    return !patron?
+            elemento[valor]
+           :
+            patron(elemento[valor]);
+  }
+  const obtenerElemento=(tabla)=>(elemento)=>{
+    return tabla.find(x=>x[elemento]==);
+  };
+
+  const buscarUniversidad=obtenerElemento=>obtenerValor;
+
+  let infoUniversidad=buscarUniversidad(basededatos.universidades)('nombre')(nombreUniversidad)(indexOf);
+  console.log('test '+JSON.stringify(infoUniversidad))
+  return
+
+  //let infoUniversidad={};
+  const universidad=basededatos.universidades.filter(universidad=>universidad.nombre.indexOf(nombreUniversidad)>-1)[0];
+  
+  if (!universidad) 
+    return undefined;
+  else infoUniversidad=universidad;
+
+  infoUniversidad.materias=basededatos.materias.filter((materia)=>materia.universidad===universidad.id);
+
+  const profesores=infoUniversidad.materias
+                   .map((materia)=>materia.profesores)
+                   .flat()
+                   .reduce((filtrado,elem)=>filtrado.includes(elem)?filtrado:[...filtrado,elem],[]);
+
+  infoUniversidad.profesores=innerJoin(basededatos.profesores,profesores);
+
+  const materiasId = infoUniversidad.materias.map((materia)=>materia.id);
+  let alumnos    = basededatos.calificaciones
+        .filter(calificacion=>materiasId.includes(calificacion.materia))
+        .map(calificacion=>calificacion.alumno)
+        .reduce((filtrado,elem)=>filtrado.includes(elem)?filtrado:[...filtrado,elem],[]);
+
+  infoUniversidad.alumnos=innerJoin(basededatos.alumnos,alumnos);
+  
+  return infoUniversidad;
 };
+
+
+/*
+
+function innerJoin(elementos,idsArray){
+  return elementos.filter(elemento=>idsArray.includes(elemento.id));
+}
+
+export const expandirInfoUniversidadByNombre = (nombreUniversidad) => {
+  // Decorador urinario 
+  const urinary = fn => {
+    return (...args) => fn(args[0]);
+  }
+
+  const indexOf=(elemento)=>(needle)=>elemento.indexOf(needle);
+  const buscarUniversidad=(universidad)=>(elemento)=>(patron)=>(valor)=>patron(universidad[elemento])(valor);
+
+  let infoUniversidad={};
+  const universidad=basededatos.universidades.filter(universidad=>universidad.nombre.indexOf(nombreUniversidad)>-1)[0];
+  
+  if (!universidad) 
+    return undefined;
+  else infoUniversidad=universidad;
+
+  infoUniversidad.materias=basededatos.materias.filter((materia)=>materia.universidad===universidad.id);
+
+  const profesores=infoUniversidad.materias
+                   .map((materia)=>materia.profesores)
+                   .flat()
+                   .reduce((filtrado,elem)=>filtrado.includes(elem)?filtrado:[...filtrado,elem],[]);
+
+  infoUniversidad.profesores=innerJoin(basededatos.profesores,profesores);
+
+  const materiasId = infoUniversidad.materias.map((materia)=>materia.id);
+  let alumnos    = basededatos.calificaciones
+        .filter(calificacion=>materiasId.includes(calificacion.materia))
+        .map(calificacion=>calificacion.alumno)
+        .reduce((filtrado,elem)=>filtrado.includes(elem)?filtrado:[...filtrado,elem],[]);
+
+  infoUniversidad.alumnos=innerJoin(basededatos.alumnos,alumnos);
+  
+  return infoUniversidad;
+};
+*********/
 
 // /**
 //  * Devuelve el promedio de edad de los alumnos.
