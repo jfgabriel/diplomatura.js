ac6ca2b9897389572962112702f3cf28c291c342
pdavid777 <66015887+pdavid777@users.noreply.github.com>
2020-06-13 00:21:15 -0300

    Exports fixed
    Funciones desplazadas hacia el archivo, exportados sólo sus nombres.
    
    Areglos varios en los returns y nombre de getFromDB que no se me había corregido

--- 002-ejercicios/src/helpers.js
+++ 002-ejercicios/src/helpers.js
@@ -6,8 +6,11 @@ function getTabla(nombreTabla){
                    database[nombreTabla]
                  :
                    nombreTabla;
-    if (!tabla) return false,console.log('Tabla no encontrada. ¿El nombre es correcto?');
-    else return tabla;
+    
+    return (tabla)?
+        tabla
+        :
+        console.log('Tabla no encontrada. ¿El nombre es correcto?');
 }
 
 //Inserte datos indicados en una tabla señalada. Bool optativo si la tabla tiene id
@@ -26,8 +29,9 @@ function insertNew(nombreTabla,datos,usesId=true){
 //1- Si viene el CAMPO busca el VALOR que en el CAMPO
 //2- Si NO VIENE CAMPO, busca por ID
 //3- Si no viene VALOR, busca todo
-function get(nombreTabla,val,campo){
+function getFromDB(nombreTabla,val,campo){
     const tabla = getTabla(nombreTabla);
+
     if (!val && campo) return console.log('Si viene el campo envíe también el valor'); //Can't send campo if not send val too
 
     return campo?
@@ -44,62 +48,75 @@ function getLastId(nombreTabla){
     return getTabla(nombreTabla).reduce((last,elem)=>
         Math.max(elem.id,last)
     ,0)
+}//o return getTabla(nombreTabla).slice(-1)[0];
+
+/////
+// -----> Funciones a exportar
+////
+
+const getUniversidades  = (id)=>getFromDB('universidades',id);
+const getProfesores     = (id)=>getFromDB('profesores',id);
+const getMaterias       = (id)=>getFromDB('materias',id);
+const getCalificaciones = (id)=>getFromDB('calificaciones',id);
+const getLastId         = (prop)=>getLastId(prop);
+const insertarProvincia = (nombre)=>{
+    const newObject=insertNew('provincias',nombre);
+    database.provincias.push(newObject);
+    console.log('Impresión de soporte para ver la nueva provincia');
+    console.log(database.provincias);
+    return newObject;
+};
+const getMateriaData    = (id)=>{//Convertido en function para llamar al this del helper
+    const materia=getMaterias(id);
+    
+    materia.universidad=getUniversidades(materia.universidad);
+    if (!materia.universidad) return 'Universidad no encontrada'
+    else materia.universidad=materia.universidad.nombre;
+
+    materia.profesores=materia.profesores.map((profesorId)=>
+        getProfesores(profesorId).nombre
+    )
+    return materia;
+};
+const getNotasDeAlumnos= function(){
+    const materias       = getMaterias();
+    const calificaciones = getCalificaciones();
+    let alumnos = getFromDB('alumnos')
+                    .map((alumno,y)=>
+                    [
+                        'ALUMNO '+(y+1),
+                        alumno.nombre.toUpperCase(),
+                        calificaciones.filter(calificacion=>calificacion.alumno===alumno.id)
+                                        .reduce((txt,calificacion)=>
+                                            txt+=`${getFromDB(materias, calificacion.materia).nombre}: ${calificacion.nota}\n`
+                                        ,'')||'Sin calificaciones'
+                    ].join('\n')
+                    ).join('\n');
+    
+
+    return  'NOTAS DE ALUMNOS\n'+
+            '----------------\n'+
+            alumnos+
+            '\n\n';
+};
+const calificarAlumnado= (nombreAlumno,nombreMateria,nota)=>{
+    const alumno = getFromDB('alumnos',nombreAlumno,'nombre')||insertNew('alumnos',nombreAlumno);
+    const materia = getFromDB('materias',nombreMateria,'nombre')||insertNew('materias',nombreMateria);
+    
+    return insertNew('calificaciones', { alumno: alumno.id, materia: materia.id, nota},false);
 }
 
 export const helpers={
-    getUniversidades:  (id)=>get('universidades',id),
-    getProfesores:     (id)=>get('profesores',id),
-    getMaterias:       (id)=>get('materias',id),
-    getCalificaciones: (id)=>get('calificaciones',id),
+    getUniversidades,
+    getProfesores,
+    getMaterias,
+    getCalificaciones,
 
-    getLastId:         (prop)=>getLastId(prop),
+    getLastId,
 
-    insertarProvincia: (nombre)=>{
-        const newObject=insertNew('provincias',nombre);
-        database.provincias.push(newObject);
-        console.log('Impresión de soporte para ver la nueva provincia');
-        console.log(database.provincias);
-        return newObject;
-    },
-
-    getMateriaData: function(id){//Convertido en function para llamar al this del helper
-        const materia=this.getMaterias(id);
-        
-        materia.universidad=this.getUniversidades(materia.universidad);
-        if (!materia.universidad) return 'Universidad no encontrada'
-        else materia.universidad=materia.universidad.nombre;
-
-        materia.profesores=materia.profesores.map((profesorId)=>
-            this.getProfesores(profesorId).nombre
-        )
-        return materia;
-    },
-    getNotasDeAlumnos: function(){
-        const materias       = this.getMaterias();
-        const calificaciones = this.getCalificaciones();
-        let alumnos = get('alumnos')
-                     .map((alumno,y)=>
-                       [
-                           'ALUMNO '+(y+1),
-                            alumno.nombre.toUpperCase(),
-                            calificaciones.filter(calificacion=>calificacion.alumno===alumno.id)
-                                          .reduce((txt,calificacion)=>
-                                                txt+=`${get(materias, calificacion.materia).nombre}: ${calificacion.nota}\n`
-                                          ,'')||'Sin calificaciones'
-                        ].join('\n')
-                      ).join('\n');
-        
-
-        return  'NOTAS DE ALUMNOS\n'+
-                '----------------\n'+
-                alumnos+
-                '\n\n';
-    },
-    calificarAlumnado: function(nombreAlumno,nombreMateria,nota){
-        const alumno = get('alumnos',nombreAlumno,'nombre')||insertNew('alumnos',nombreAlumno);
-        const materia = get('materias',nombreMateria,'nombre')||insertNew('materias',nombreMateria);
-        
-        return insertNew('calificaciones', { alumno: alumno.id, materia: materia.id, nota},false);
-    }
+    insertarProvincia,
+    getMateriaData,
+    getNotasDeAlumnos,
+    calificarAlumnado
 }
 
