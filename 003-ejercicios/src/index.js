8b49c9543c06f451517c531eea255c4e4b707c48
Juan <juan@MacBook-Pro.local>
2020-06-21 11:02:36 -0300

    Agregar ejercicios 003

---
+++ 003-ejercicios/src/index.js
@@ -0,0 +1,18 @@
+// 1) Collection
+// Escribir una clase Collection que representa una colección de elementos. Esta clase debe ...
+// - Poder construirse o vacía (sin elementos) o con una lista inicial de elemenetos
+// - Debe tener un método 'add' para poder agregar un elemento
+// - Debe tener un método 'delete' para poder eliminar un elemento
+// - Debe tener un método 'has' para poder determinar un elemento existe en la colección
+
+// 2) TAREA PARA EL HOGAR (no realizar en clase): Vector
+// Escribir una clase Vector que represente un espacio de dos dimensiones x e y. Esta clase debe ...
+// - Poder construirse desde dos valores 'x' e 'y'
+// - Debe tener dos getters para obtener esos valores
+// - Debe tener un método 'sumar' que tome un vector por parámetro y devuelva *un nuevo vector* sumando los valores de x e y de ambos vectores
+//   Por ejemplo
+//    console.log(new Vector(1, 2).sumar(new Vector(2, 3)));
+
+// 3) Ejercicio 3
+
+//
