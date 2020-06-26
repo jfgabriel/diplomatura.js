7a56078c00e9b464c3b56ce35f283c4e3f563599
jotero <jotero@jotero>
2020-06-26 20:38:02 -0300

    fix: se mueven ejercicios 2 a la carpeta de grupo

--- grupos/grupo2/002-ejercicios/src/index.js
+++ grupos/grupo2/002-ejercicios/src/index.js
@@ -52,6 +52,21 @@ function insertProvincia(name) {
 }
 // 10) Implementar una función que reciba el id de una materia y devuelva la materia son los ids de universidad y profesores resueltos a sus nombres
 
+function getMateria(idMateria) {
+    let materia = (database.materias.find(elem => elem.id === idMateria));
+    let idProfesores = materia.profesores;
+    let profesores = '';
+    let resultados = [];
+    let prof = [];
+    idProfesores.forEach(idP => {
+        profesores = (database.profesores.find(elem => elem.id === idP));
+        prof.push(profesores.nombre);
+    });
+    resultados.push({ materia: materia.nombre, universidad: (database.universidades.find(elem => elem.id === materia.universidad).nombre), profesores: prof });
+
+    return resultados;
+}
+
 // 11) Implementar una función que muestre en consola la información para todos los alumnos de la siguiente manera:
 // NOTAS DE ALUMNOS
 // ----------------
@@ -61,6 +76,44 @@ function insertProvincia(name) {
 // ALUMNO 2
 // ...
 
+function mostrarDatos() {
+    let calificaciones = database.calificaciones;
+    let resultados = [];
+    calificaciones.forEach(calificacion => {
+        let nuevo = new Object();
+        nuevo.alumno = database.alumnos.find(alumno => alumno.id === calificacion.alumno).nombre;
+        nuevo.materia = database.materias.find(materia => materia.id === calificacion.materia).nombre;
+        nuevo.nota = calificacion.nota;
+        resultados.push(nuevo);
+    })
+    resultados.forEach(resultado => {
+        console.log(' \nNOTAS DE ALUMNOS');
+        console.log('----------------');
+        console.log(resultado.alumno.toString().toUpperCase());
+        console.log(resultado.materia, ': ', resultado.nota);
+    });
+}
+
 // 12) Implementar una función que guarde la calificación de un alumno y una materia
 //     La función recibirá: 'nombre del alumno', 'nombre de la materia', 'nota'
 //     Si el alumno y/o la materia no existen deberán crearlos en sus respectivas tablas
+
+function notaAlumno(nombreAlumno, nombreMateria, notaAlumno) {
+    let idAlumno;
+    let idMateria;
+    let materia = database.materias.find(materia => materia.nombre === nombreMateria);
+    if (!materia) {
+        idMateria = helpers.ultimoId(database.materias) + 1
+        database.materias.push({ id: idMateria, nombre: nombreMateria });
+    } else {
+        idMateria = materia.id;
+    }
+    let alumno = database.alumnos.find(alumno => alumno.nombre === nombreAlumno);
+    if (!alumno) {
+        idAlumno = helpers.ultimoId(database.alumnos) + 1;
+        database.alumnos.push({ id: idAlumno, nombre: nombreAlumno });
+    } else {
+        idAlumno = alumno.id;
+    }
+    database.calificaciones.push({ alumno: idAlumno, materia: idMateria, nota: notaAlumno });
+}
