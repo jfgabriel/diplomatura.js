81eec2a476a6c267d53c2f8f49943bb5cbc22bac
Cristian Gavilan <cristiangavilan@gmail.com>
2020-06-26 18:47:55 -0300

    001 cristiangavilan (#8)
    * 002-cristiangavilan
    
    * 001-cristiangavilan
    
    * revert 002
    
    * Update grupos/grupo4/001-ejercicios/src/moduloEjercicios.js
    
    Co-authored-by: Eduardo Cuomo <reduardo7@gmail.com>

--- grupos/grupo4/001-ejercicios/src/index.js
+++ grupos/grupo4/001-ejercicios/src/index.js
@@ -1,11 +1,16 @@
 import {
   materiasAprobadasByNombreAlumno,
   expandirInfoUniversidadByNombre,
+  promedioDeEdad,
+  alumnosConPromedioMayorA,
+  materiasSinAlumnosAnotados,
+  promedioDeEdadByUniversidadId
 } from './moduloEjercicios';
 
 import baseDeDatos from './basededatos';
 
 // materiasAprobadasByNombreAlumno
+
 console.log('------------------------------------------------------');
 console.log('Ejecutando ejercicios materiasAprobadasByNombreAlumno.');
 
@@ -20,6 +25,7 @@ const materiasAprobadasPorAlina = materiasAprobadasByNombreAlumno(
 console.log('Materias aprobadas por Alina:', materiasAprobadasPorAlina);
 
 // expandirInfoUniversidadByNombre
+
 console.log('------------------------------------------------------');
 console.log('Ejecutando ejercicios expandirInfoUniversidadByNombre.');
 
@@ -32,3 +38,38 @@ const infoUniversidadRio = expandirInfoUniversidadByNombre(
   'Universidad de Rio Negro'
 );
 console.log('Info rio negro:', infoUniversidadRio);
+
+// promedioDeEdad
+
+console.log('------------------------------------------------------');
+console.log('Ejecutando ejercicio promedioDeEdad.');
+
+const promedioEdad = promedioDeEdad();
+console.log('Edad Promedio:', promedioEdad);
+
+// alumnosConPromedioMayorA
+
+console.log('------------------------------------------------------');
+console.log('Ejecutando ejercicio alumnosConPromedioMayorA.');
+
+const alumnosPromedioMayorA = alumnosConPromedioMayorA(1);
+console.log('alumnosConPromedioMayorA(1):', alumnosPromedioMayorA);
+
+// materiasSinAlumnosAnotados
+
+console.log('------------------------------------------------------');
+console.log('Ejecutando ejercicio materiasSinAlumnosAnotados.');
+
+const materiasSinAlumnos = materiasSinAlumnosAnotados();
+console.log('materiasSinAlumnosAnotados():', materiasSinAlumnos);
+
+// promedioDeEdadByUniversidadId
+
+console.log('------------------------------------------------------');
+console.log('Ejecutando ejercicio promedioDeEdadByUniversidadId.');
+
+const promedioDeEdadByUniversidad = promedioDeEdadByUniversidadId(1);
+console.log('promedioDeEdadByUniversidadId(1):', promedioDeEdadByUniversidad);
+
+const promedioDeEdadByUniversidad2 = promedioDeEdadByUniversidadId(2);
+console.log('promedioDeEdadByUniversidadId(2):', promedioDeEdadByUniversidad2);
--- grupos/grupo4/001-ejercicios/src/moduloEjercicios.js
+++ grupos/grupo4/001-ejercicios/src/moduloEjercicios.js
@@ -19,12 +19,45 @@ import basededatos from './basededatos';
       universidad: 2,
     }
   ]
- * @param {nombreAlumno} nombreAlumno
+ * @param {string} nombreAlumno el id del alumno
  */
 export const materiasAprobadasByNombreAlumno = (nombreAlumno) => {
   // Ejemplo de como accedo a datos dentro de la base de datos
-  // console.log(basededatos.alumnos);
-  return [];
+  //console.log(basededatos.alumnos);
+  let retorno = [];
+  // Primera resolución
+  // basededatos.alumnos.forEach( alumno => {
+  //   if(alumno.nombre === nombreAlumno)
+  //     {
+  //       //console.log(alumno);
+  //       basededatos.calificaciones.forEach( calificacion => {
+  //         if ((calificacion.alumno === alumno.id) && (calificacion.nota >= 4))
+  //            {
+  //              //console.log(calificacion)
+  //              basededatos.materias.forEach( materia => {
+  //                if (materia.id === calificacion.materia) 
+  //                 //console.log(materia);
+  //                 retorno.push(materia)
+  //               });
+  //            }
+  //       });
+  //     }
+  // });
+  // return retorno;
+
+
+  // Segunda resolución
+
+  // Busco el alumno
+  const alumno = basededatos.alumnos.filter(alumno => alumno.nombre === nombreAlumno);
+  // Busco las calificaciones aprobadas de ese alumno
+  const calificaciones = basededatos.calificaciones.filter(calificacion => (calificacion.alumno === alumno[0].id) && (calificacion.nota >= 4));
+  // Por cada materia aprobada busco el objeto completo
+  calificaciones.forEach(calificacion => {
+    retorno.push(basededatos.materias.find(materia => materia.id === calificacion.materia));
+  });
+  // Devuelvo el resultado
+  return retorno;
 };
 
 /**
@@ -66,39 +99,153 @@ export const materiasAprobadasByNombreAlumno = (nombreAlumno) => {
          { id: 2, nombre: 'Alina Robles', edad: 21, provincia: 2 },
       ]
     }
- * @param {string} nombreUniversidad
- */
+    * @param {string} nombreUniversidad
+    */
+
 export const expandirInfoUniversidadByNombre = (nombreUniversidad) => {
-  return {};
+  // Recolectores de datos
+  let materias = [];
+  let profesores = [];
+  let alumnos = [];
+  //Recupero el objeto Universidad
+  let universidad = basededatos.universidades.find(univ => univ.nombre === nombreUniversidad);
+  // Si no es undefined busco el resto de info
+  if (universidad) {
+    //Filtro las materias de esa universidad
+    materias = basededatos.materias.filter(materia => materia.universidad === universidad.id);
+    //Por cada materia busco el resto de la informacion
+    materias.forEach(materia => {
+      // Busco los profesores
+      materia.profesores.forEach(profId => {
+        // Por cada profesor encontrado
+        let profesorFound = basededatos.profesores.find(prof => prof.id === profId);
+        //verifico que no esté ya incluido y lo agrego
+        if (!profesores.includes(profesorFound)) {
+          profesores.push(profesorFound);
+        }
+        // Recorro las calificaciones
+        basededatos.calificaciones.forEach(calificacion => {
+          // por cada Materia encontrada en calificaciones
+          if (calificacion.materia === materia.id) {
+            // busco el alumno de esa calificacion
+            let alumnoXMateria = basededatos.alumnos.find(alumno => alumno.id === calificacion.alumno);
+            //verifico que no esté ya incluido y lo agrego
+            if (!alumnos.includes(alumnoXMateria)) {
+              alumnos.push(alumnoXMateria);
+            }
+          }
+        });
+      });
+    });
+  }
+  // presento la información utilizando el operador spread
+  return {
+    ...universidad,
+    materias: materias,
+    profesores: profesores,
+    alumnos: alumnos
+  };
 };
 
 // /**
 //  * Devuelve el promedio de edad de los alumnos.
 //  */
-// export const promedioDeEdad = () => {
-//   return [];
-// };
+export const promedioDeEdad = () => {
+  // para sumar las edades
+  let partialEdades = 0;
+  // recorro el array de alumnos, uso la edad de cada uno e incremento la cantidad
+  basededatos.alumnos.forEach(alumno => {
+    partialEdades += alumno.edad;
+  });
+  // presento el resultado
+  return [partialEdades / basededatos.alumnos.length];
+};
 
 // /**
 //  * Devuelve la lista de alumnos con promedio mayor al numero pasado
 //  * por parametro.
 //  * @param {number} promedio
 //  */
-// export const alumnosConPromedioMayorA = (promedio) => {
-//   return [];
-// };
+export const alumnosConPromedioMayorA = (promedioIn) => {
+  // Para sacar el resultado
+  let resultado = [];
+  // Recorro los alumnos
+  basededatos.alumnos.forEach(alumno => {
+    // Busco las calificaciones por cada alumno
+    let calificacionesXAlumno = basededatos.calificaciones.filter(calificacion => calificacion.alumno === alumno.id);
+    // Inicializo la suma parcial
+    let sumaParcial = 0;
+    // Sumo las calificaciones por alumno
+    calificacionesXAlumno.forEach(calif => {
+      sumaParcial += calif.nota
+    });
+    // Obtengo el promedio
+    let promedio = sumaParcial / calificacionesXAlumno.length;
+    // Si supera el promedio ingresado lo pongo en resultado para mostrar
+    if (promedio > promedioIn) {
+      resultado.push({ ...alumno, promedio: promedio });
+    }
+  });
+  // Presento resultado.
+  return resultado;
+};
 
 // /**
 //  * Devuelve la lista de materias sin alumnos
 //  */
-// export const materiasSinAlumnosAnotados = () => {
-//   return [];
-// };
+export const materiasSinAlumnosAnotados = () => {
+  // Para sacar el resultado
+  let resultado = [];
+  // Recorro las materias
+  basededatos.materias.forEach(materia => {
+    // Busco en las calificaciones cuantos almunos rindieron
+    let calificaciones = basededatos.calificaciones.filter(calificacion => calificacion.materia === materia.id);
+
+    //En el caso de querer la data completa de inscriptos por materia
+    // resultado.push({...materia, inscriptos: calificaciones.length});
+
+    // Verifico que no tenga inscriptos
+    if (calificaciones.length === 0) {
+      resultado.push(materia);
+    }
+  });
+  // Presento resultado.
+  return resultado;
+};
 
 // /**
 //  * Devuelve el promdedio de edad segun el id de la universidad.
 //  * @param {number} universidadId
 //  */
-// export const promedioDeEdadByUniversidadId = (universidadId) => {
-//   return [];
-// };
+export const promedioDeEdadByUniversidadId = (universidadId) => {
+  // Para cargar los alumnos
+  let alumnos = [];
+  // Busco las materias de la universidadId
+  basededatos.materias.filter(materia => {
+    if (materia.universidad === universidadId) {
+      // Busco las calificacion de dicha materia
+      basededatos.calificaciones.filter(calificacion => {
+        if (calificacion.materia === materia.id) {
+          // Busco los alumnos con calificaciones en esas materias    
+          basededatos.alumnos.filter(alumno => {
+            if (alumno.id === calificacion.alumno) {
+              // Cargo en este arreglo los alumnos de esas materias de la universidadId
+              if (!alumnos.includes(alumno)) {
+                alumnos.push(alumno)
+              }
+            }
+          });
+        }
+      });
+    }
+  });
+  // Calculo el promedio
+  let sumaParcial = 0;
+  alumnos.forEach(alumno => {
+    // Ver los alumnos
+    // console.log(alumno);
+    sumaParcial += alumno.edad;
+  });
+  // Presento resultado.
+  return [sumaParcial / alumnos.length];
+};
