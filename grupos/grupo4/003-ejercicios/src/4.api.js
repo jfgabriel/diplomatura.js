5dbf6dcaa191262c63c743b88163ee88852ef125
majorotter <65582299+majorotter@users.noreply.github.com>
2020-06-29 11:20:29 -0300

    Update 4.api.js

--- grupos/grupo4/003-ejercicios/src/4.api.js
+++ grupos/grupo4/003-ejercicios/src/4.api.js
@@ -1,20 +1,21 @@
 import fetch from 'node-fetch';
 
-// fetch('..... aquí va la url')
-// recordar que fetch devuelve una promise, entonces puede hacer fetch(....).then(resultado => {.... })
-
 export const testApi = {
   test() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((resultado) => resultado.json()) // equivalente a: function(resultado){return resultado.json()}
-      .then((json) => {
-        const cantUsuarios = Object.keys(json).length;
-        console.log('Cantidad de Usuarios: ' + cantUsuarios);
-        json.forEach((element) => {
-          console.log('  ' + element.name + ' vive en ' + element.address.city);
-        });
+      .then((json) =>
+        json.map(({ name, address }) => ({
+          name,
+          city: address.city,
+        }))
+      )
+      .then((jsonUsuarios) => {
+        const cantUsuarios = Object.keys(jsonUsuarios).length;
+        console.log('Cantidad de Usuarios:', cantUsuarios);
+        console.log(jsonUsuarios);
       })
-      .catch((error) => console.log(error))
+      .catch((error) => console.error(error))
       .finally(() =>
         console.log(
           'Finalmente https://jsonplaceholder.typicode.com/users  en 4.api.js concluyó'
@@ -22,3 +23,4 @@ export const testApi = {
       );
   },
 };
+
