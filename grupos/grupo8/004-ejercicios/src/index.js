acd97b1f10776c88f432ae2a37c3846e878073b1
Felipe Vidal Soto <felipe.vidal@est.fi.uncoma.edu.ar>
2020-07-05 19:03:39 -0300

    Subo ejercicio4

--- grupos/grupo8/004-ejercicios/src/index.js
+++ grupos/grupo8/004-ejercicios/src/index.js
@@ -2,16 +2,22 @@ import express from 'express';
 const app = express();
 import postsRoutes from './controllers/posts';
 import albumRoutes from './controllers/albums';
+import userRoutes from './controllers/userInfo';
 
 const PORT = 8080;
 
 app.use('/posts', postsRoutes);
 app.use('/albums', albumRoutes);
+app.use('/userInfo', userRoutes);
 
-// Implementar el endpoint de stats aca. GET "/"
+//Implementar el endpoint de stats aca. GET "/"
 app.get('/', function (req, res) {
   res.json({ mensaje: 'Hello world!' });
 });
 
+
+
+
 app.listen(PORT);
 console.log(`Express started on port ${PORT}`);
+
