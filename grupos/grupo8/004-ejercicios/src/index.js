5f8e92db756b55033141f88369cc616277c70b05
MaximilianoVillalba <maximiliano.villalba@est.fi.uncoma.edu.ar>
2020-07-05 18:24:53 -0300

    Ejercicio 1 finalizado

--- grupos/grupo8/004-ejercicios/src/index.js
+++ grupos/grupo8/004-ejercicios/src/index.js
@@ -5,7 +5,7 @@ import albumRoutes from './controllers/albums';
 const app = express();
 const m = moment();
 const os = require('os');
-m.locale('es-AR');
+m.locale('es-AR'); //Setea el calendario a español y horario Argentino
 
 const PORT = 8080;
 
@@ -15,15 +15,15 @@ app.use('/albums', albumRoutes);
 // Implementar el endpoint de stats aca. GET "/"
 app.get('/', function (req, res) {
   const result = {
-      serverCurrentTime: new Date(),
-      serverStartUpTime: m.format('MMMM Do YYYY, h:mm:ss a'),
-      serverUpTime: m.startOf('minute').fromNow(),
+      serverCurrentTime: new Date(), //Crea la fecha actual
+      serverStartUpTime: m.format('MMMM Do YYYY, h:mm:ss a'), //Formatea la fecha y hora de la manera enviada por parametro
+      serverUpTime: m.startOf('minute').fromNow(), //Obtiene el tiempo en minutos desde el momento que se ejecuto el moment
       status: {
-        freemem: os.freemem(),
-        totalmem: os.totalmem(),
-        uptime: os.uptime(),
-        hostname: os.hostname(),
-        platform: os.platform(),
+        freemem: os.freemem(), //Devuelva la cantidad de memoria libre en el sistema en formato bytes
+        totalmem: os.totalmem(), //Devuelve la cantidad total de memoria del sistema en bytes
+        uptime: os.uptime(), //Devuelve el tiempo de actividad del sistema en segundos
+        hostname: os.hostname(), //Devuelve el nombre del equipo del sistema operativo como una cadena.
+        platform: os.platform(), //Devuelve una cadena que identifica la plataforma del sistema operativo
       },
     };
     res.json(result);
