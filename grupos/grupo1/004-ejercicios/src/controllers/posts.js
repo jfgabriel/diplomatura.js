d184755dd6185245aea45678e43712029603e9f4
Diego Perlo <perlodiego@gmail.com>
2020-07-12 12:39:12 -0300

    resolve ejercicios 4

--- grupos/grupo1/004-ejercicios/src/controllers/posts.js
+++ grupos/grupo1/004-ejercicios/src/controllers/posts.js
@@ -1,13 +1,62 @@
 import express from 'express';
+import fetch, { Body } from 'node-fetch';
+let postApi = express.Router();
 
-var postApi = express.Router();
+postApi.get('/', async function (req, res) {
+  try {
+    let posts = await fetch(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    let postsJson = await posts.json();
 
-postApi.get('/', function (req, res) {
-  res.send('Hola posts!.');
+    let users = await fetch(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    let usersJson = await users.json();
+
+    let response = await postsJson.map((post) => {
+      return {
+        id: post.id,
+        title: post.title,
+        body: post.body,
+        user: usersJson.find((u) => u.id === post.userId),
+      };
+    });
+
+    res.send(response);
+  } catch (e) {
+    return e;
+  }
 });
 
-postApi.get('/:id', function (req, res) {
-  res.send(`Hola post ${req.params.id}.`);
+postApi.get('/:id', async function (req, res) {
+  try {
+    let postId = req.params.id;
+    let post = await fetch(
+      'https://jsonplaceholder.typicode.com/posts/' + postId
+    );
+    let postJson = await post.json();
+
+    let comments = await fetch(
+      'https://jsonplaceholder.typicode.com/posts/' + postId + '/comments'
+    );
+    let commentsJson = await comments.json();
+
+    let response = postJson;
+
+    response.posts = await commentsJson.map((comments) => {
+      return {
+        id: comments.id,
+        name: comments.name,
+        email: comments.email,
+        body: comments.body,
+      };
+    });
+
+    res.send(response);
+  } catch (e) {
+    return e;
+  }
 });
 
 export default postApi;
