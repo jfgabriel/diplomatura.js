3774ac4e78d4541ec352d06ccc93803dfc2858a6
Diego Perlo <perlodiego@gmail.com>
2020-07-13 10:49:06 -0300

    amend

--- grupos/grupo1/003-ejercicios/src/6.delay.js
+++ grupos/grupo1/003-ejercicios/src/6.delay.js
@@ -1,17 +1,16 @@
 export const delay = (mensaje, tiempo) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        console.log(mensaje);
-        }, tiempo);
-    });
-  };
-  
-  export async function run() {
-    console.log(1);
-    delay('Terminó 1', 1000).then('Terminó 1');
-    console.log(2);
-    delay('Terminó 2', 2000).then('Terminó 2');
-    console.log(3);
-    delay('Terminó 3', 3000).then('Terminó 3');
-  };
-  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(mensaje);
+    }, tiempo);
+  });
+};
+
+export async function run() {
+  console.log(1);
+  delay('Terminó 1', 1000).then('Terminó 1');
+  console.log(2);
+  delay('Terminó 2', 2000).then('Terminó 2');
+  console.log(3);
+  delay('Terminó 3', 3000).then('Terminó 3');
+}
