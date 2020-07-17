b45ac27b0650ecd8eba6a9d0378134148fcdb78e
jose troche <tjl_troche@hotmail.com>
2020-07-16 23:48:46 -0300

    primer commit

--- grupos/grupo5/005-ejercicios/src/controllers/alumnos.js
+++ grupos/grupo5/005-ejercicios/src/controllers/alumnos.js
@@ -4,23 +4,54 @@ import {connect} from '../connection'; //Importamos el metodo connect
 
 const router = express.Router();
 
+//consultar todo los alumnos
 router.get('/', async (req, res) => {
   try{
     const db = await connect(); //Me devuelve una conexion a la base de datos
     const salida = await db.collection('alumnos').find({}).toArray();
-    res.json({salida});
+    if (!salida){
+      res.status(404).json({
+        message: 'Error obtenido de la lista de Alumnos'
+        //https://expressjs.com/es/guide/error-handling.html
+        //http://ottocol.github.io/ADI_1516/practicas/2_intro_Node/enunciado.html
+      })
+    }
+    res.json(salida);
   }
   catch(e){
     console.log(e);
   }
 });
 
+//obtener un alumno por id
 router.get('/:id', async (req, res) => {
   try{
-    const id = parseInt(req.params.id); //es un string
+   const id = parseInt(req.params.id); //es un string
     const db = await connect(); //Me devuelve una conexion a la base de datos
-    const salida = await db.collection('alumnos').findOne({'id':id});
-    res.json(salida);
+    const salida = await db.collection('alumnos').findOne({'id':id}); 
+    if (!salida){
+      res.status(404).json({
+          message: 'No se registra el Alumno'
+        })
+     }
+  res.json({salida});
+  }
+  catch(e){
+    console.log(e);
+  }
+});
+
+router.get('/:nombre', async (req, res) => {
+  try{
+   const nombre =   req.params.nombre; //es un string
+    const db = await connect(); //Me devuelve una conexion a la base de datos
+    const salida = await db.collection('alumnos').find({'nombre': /nombre/}).toArray();   
+    if (!salida){
+      res.status(404).json({
+          message: 'No se registra el Alumno'
+        })
+     }
+  res.json({salida});
   }
   catch(e){
     console.log(e);
@@ -42,6 +73,11 @@ router.post('/', async (req, res) => {
       provincia:req.body.provincia
     };
     const salida = await db.collection('alumnos').insertOne(alumno);
+    if (!salida){
+      res.status(404).json({
+          message: 'No se pudo insertar el Alumno'
+        })
+     }
     res.json(salida.ops[0]);
   }
   catch(e){
@@ -57,14 +93,40 @@ router.delete('/:id', async (req, res) => {
   const id = parseInt(req.params.id); 
   const db = await connect();
   const salida = await db.collection('alumnos').deleteOne({'id':id});
+  if (!salida){
+    res.status(404).json({
+        message: 'No se pudo eliminar el Alumno'
+      })
+   }
+   if (salida){
+    res.status(200).json({
+        message: 'ok:true'
+      })
+   }
   res.json(salida);
-  
-  //onsole.log(salida);
   }
   catch(error){
     console.log(error);
   }
 
+});
+
+//actualizar alumno.-
+router.put('/:id', async (req, res) => {
+  try{
+  const id = parseInt(req.params.id); 
+  const updateAlumno ={
+    nombre:req.body.nombre,
+    edad:req.body.edad
+  };
+  const db = await connect();
+  await db.collection('alumnos').updateOne({'id':id}, {$set:updateAlumno});
+  const salida = await db.collection('alumnos').findOne({'id':id});
+  res.json({salida});
+  }
+  catch(error){
+    console.log(error);
+  }
 });
 
 // Completar el resto de los métodos
