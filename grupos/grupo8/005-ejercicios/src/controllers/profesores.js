d0471a1b8674e0ffcf45e400776d32626ff3be64
waltergarrido <blackwalter7@gmail.com>
2020-07-19 19:34:32 -0300

    helpers2

--- grupos/grupo8/005-ejercicios/src/controllers/profesores.js
+++ grupos/grupo8/005-ejercicios/src/controllers/profesores.js
@@ -1,46 +1,57 @@
 import express from 'express';
-import { connect } from '../connection';
+import { helpers } from '../helpers';
 
 
 const router = express.Router();
 
 
 router.get('/', async function (req, res) {
-    const db = await connect();
-    const result = await db.collection('profesores').find({}).toArray();
+    const result = await helpers.getCollection('profesores');
     res.json(result);
 });
 
-router.get('/:nombre', async function (req, res) {
-    const db = await connect();
-    console.log(req.params.nombre);
-    const result = await db.collection('profesores').find({ 'nombre': req.params.nombre }).toArray();
+router.get('/:id', async function (req, res) {
+
+    const idProfesor = parseInt(req.params.id);
+    const result = await helpers.getCollectionId(idProfesor, 'profesores');
     // Completar
     res.json(result);
 });
 
 router.post('/', async function (req, res) {
-    const db = await connect();
+
     const dato = {
         id: parseInt(req.body.id),
         nombre: req.body.nombre
     };
 
-    const datoInsert = await db.collection('profesores').insert(dato);
+    const datoInsert = await helpers.postCollection(
+        dato,
+        'profesores'
+    );
     res.json(datoInsert);
 });
 
 router.put('/:id', async function (req, res) {
-    const db = await connect();
-    const result = await db.collection('profesores').update({ 'id': parseInt(req.params.id) }, { $set: { 'nombre': req.body.nombre } });
-    res.json(result);
+
+    const idProfe = parseInt(req.params.id);
+    const datos = req.body;
+    const resultUpdate = await helpers.putCollection(
+        idProfe,
+        datos,
+        'profesores'
+    );
+    res.json(resultUpdate);
 });
 
 router.delete('/:id', async function (req, res) {
-    const db = await connect();
-    const result = await db.collection('profesores').deleteOne({ 'id': parseInt(req.params.id) });
+    const idProfe = parseInt(req.params.id);
+    const resultDelete = await helpers.deleteDocument(
+        idProfe,
+        'profesores'
+    );
 
-    result.deletedCount > 0 ? res.json({ ok: true }) : res.json({ ok: false });
+    res.json({ ok: resultDelete.deletedCount > 0 });
 });
 
 // Completar el resto de los métodos
