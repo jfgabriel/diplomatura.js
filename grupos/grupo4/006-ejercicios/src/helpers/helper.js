3abec9c044c8a19c50c771b3e65847f80397893d
Eduardo Cuomo <reduardo7@gmail.com>
2020-07-23 20:12:26 -0300

    006 fixes (#17)

--- grupos/grupo4/006-ejercicios/src/helpers/helper.js
+++ grupos/grupo4/006-ejercicios/src/helpers/helper.js
@@ -8,72 +8,57 @@ export const MOUNTAINS = [
   { name: 'Mont Blanc', height: 4808, place: 'Italy/France' },
 ];
 
+const TEXT_CONVERTER = {
+  name: 'nombre',
+  height: 'altura',
+  place: 'lugar',
+};
+
 const traducir = (word) => {
-  const objConverter = {
-    name: 'nombre',
-    height: 'altura',
-    place: 'lugar',
-  };
-  const palabra = objConverter[word];
-  return palabra;
+  return TEXT_CONVERTER[word];
 };
 
 export const arrayToTable = (arreglo, document, traducirEncabezado = false) => {
-  let tabla = document.createElement('table');
-  let cuerpoTabla = document.createElement('tbody');
+  const tabla = document.createElement('table');
+  const cuerpoTabla = document.createElement('tbody');
   let agregarEncabezado = true; //bandera obtener el encabezado del 1er objeto del arreglo
-  let encabezado = document.createElement('tr');
+  const encabezado = document.createElement('tr');
+
   arreglo.forEach((objeto) => {
-    let fila = document.createElement('tr');
-    for (let key in objeto) {
+    const fila = document.createElement('tr');
+
+    for (const key in objeto) {
       if (agregarEncabezado) {
         let keyEncabezado = key;
-        let celdaEncabezado = document.createElement('th');
+        const celdaEncabezado = document.createElement('td');
+
         if (traducirEncabezado) {
           keyEncabezado = traducir(keyEncabezado);
         }
+
         const celdaTextoEncabezado = document.createTextNode(
           `${keyEncabezado}`
         );
+
         celdaEncabezado.appendChild(celdaTextoEncabezado);
         encabezado.appendChild(celdaEncabezado);
       }
-      let celdaFila = document.createElement('th');
+
+      const celdaFila = document.createElement('td');
       const celdaTextoFila = document.createTextNode(`${objeto[key]}`);
       celdaFila.appendChild(celdaTextoFila);
       fila.appendChild(celdaFila);
     }
+
     if (agregarEncabezado) {
       agregarEncabezado = false;
       cuerpoTabla.appendChild(encabezado);
     }
+
     cuerpoTabla.appendChild(fila);
   });
+
   tabla.appendChild(cuerpoTabla);
-
-  return tabla;
-};
-
-export const arrayToTableString = (arreglo) => {
-  let agregarEncabezado = true;
-  let encabezado = '<tr>';
-  let cuerpo = '';
-  arreglo.forEach((json) => {
-    let fila = '<tr>';
-    for (const key in json) {
-      if (agregarEncabezado) {
-        encabezado += `<th>${key}</th>`;
-      }
-      fila += `<th>${json[key]}</th>`;
-    }
-    fila += '</tr>';
-    if (agregarEncabezado) {
-      encabezado += '</tr>';
-      agregarEncabezado = false;
-    }
-    cuerpo += fila;
-  });
-  const tabla = '<table>' + encabezado + cuerpo + '</table>';
 
   return tabla;
 };
