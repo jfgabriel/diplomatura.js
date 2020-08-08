c2538f3621a0f24a42199993a2c4d7d54feac2fe
Marcelo Moyano <marcelo.moyano@fi.uncoma.edu.ar>
2020-08-08 18:48:48 -0300

    Ajustes imagen

--- grupos/grupo7/007-ejercicios/universidad-app/src/componentes/AppInicial.js
+++ grupos/grupo7/007-ejercicios/universidad-app/src/componentes/AppInicial.js
@@ -1,5 +1,11 @@
 import React from "react";
+import esquema from "../images/esquema.png";
 
 export default function AppInicial(props) {
-  return <h1>Bienvenido al Sistema de Universidades</h1>;
+  return (
+    <>
+      <h1>Bienvenido al Sistema de Universidades</h1>
+      <img src={esquema} alt="Esquema" />
+    </>
+  );
 }
