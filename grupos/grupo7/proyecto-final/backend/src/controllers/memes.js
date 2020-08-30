cdd0f86dcfbdaddddb00ec078d2def7dd68445f0
Juan Calou <juan.calou@patagonian.it>
2020-08-30 20:01:29 -0300

    updated coleccion

--- grupos/grupo7/proyecto-final/backend/src/controllers/memes.js
+++ grupos/grupo7/proyecto-final/backend/src/controllers/memes.js
@@ -42,7 +42,7 @@ router.get('/', async function (req, res) {
 
   const memes = await helpers.getDataFilterByCondition(
     db,
-    'memes',
+    coleccion,
     condition,
     proyection,
     sorting,
