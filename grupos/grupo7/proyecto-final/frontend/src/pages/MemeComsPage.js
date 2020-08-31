61e54e5e885081b09278b637eb7c59a68d64f766
Romina Bertini <romiber@gmail.com>
2020-08-30 22:45:11 -0300

    Ajustes en componente comentarios

--- grupos/grupo7/proyecto-final/frontend/src/pages/MemeComsPage.js
+++ grupos/grupo7/proyecto-final/frontend/src/pages/MemeComsPage.js
@@ -18,31 +18,7 @@ export default class MemeComsPage extends Component {
           <div className="row">
             <div className="col-12">
               <h1>Comments</h1>
-              <MemeComs
-                userName={this.state.loggedin}
-                comments={[
-                  {
-                    id: 1,
-                    author: "Jacinto",
-                    date: "30/08/2020 17:00",
-                    comment:
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et tincidunt nisl, vitae lacinia augue",
-                  },
-                  {
-                    id: 2,
-                    author: "Pablito",
-                    date: "30/08/2020 17:00",
-                    comment: "Nulla efficitur sodales porttitor",
-                  },
-                  {
-                    id: 3,
-                    author: "Laura",
-                    date: "30/08/2020 17:00",
-                    comment:
-                      "Phasellus ultrices scelerisque risus ac vestibulum",
-                  },
-                ]}
-              />
+              <MemeComs userName={this.state.loggedin} />
             </div>
           </div>
         </div>
