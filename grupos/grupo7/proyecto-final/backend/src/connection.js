51db4c983790038598b7fdbd7c08559a42275f43
Marcelo Moyano <marcelo.moyano@fi.uncoma.edu.ar>
2020-09-07 19:17:22 -0300

    User Schema, Model y Controller

--- grupos/grupo7/proyecto-final/backend/src/connection.js
+++ grupos/grupo7/proyecto-final/backend/src/connection.js
@@ -1,4 +1,3 @@
-import { MongoClient } from 'mongodb';
 import mongoose from 'mongoose';
 
 export async function connect() {
--- grupos/grupo7/proyecto-final/backend/src/controllers/user.js
+++ grupos/grupo7/proyecto-final/backend/src/controllers/user.js
@@ -1,31 +1,26 @@
 import express from 'express';
-import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { helpers } from '../db_helpers.js';
+import UserModel from '../models/usuario';
 
 const router = express.Router();
-const coleccion = 'usuario';
 
 router.post('/login', async (req, res) => {
-  // Token
-  const db = req.app.locals.db;
-  const user = await helpers.getDataFilterByName(
-    db,
-    coleccion,
-    req.body.username
-  );
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username });
+
   if (user) {
-    bcrypt.compare(req.body.password, user.password, (err, isValid) => {
+    bcrypt.compare(password, user.password, (err, isValid) => {
       if (isValid) {
         const token = jwt.sign(
           {
-            username: req.body.username,
-            expire: Date.now() + 1000 * 60 * 60 * 24 * 1, // 1 day
+            username,
+            expire: Date.now() + 1000 * 60 * 60 * 24 * 7, // 7 day
           },
           process.env.JWT_SECRET
         );
-        res.json({ login: 'ok', token: token, username: req.body.username });
+        res.json({ login: 'ok', token: token, username });
       } else {
         res.send('Password incorrecto');
       }
@@ -36,29 +31,27 @@ router.post('/login', async (req, res) => {
 });
 
 router.route('/register').post(async (req, res, next) => {
-  const hash = bcrypt.hashSync(req.body.password, 10);
-  const db = req.app.locals.db;
-  const user = await helpers.getDataFilterByName(
-    db,
-    coleccion,
-    req.body.username
-  );
+  const { username, password, email } = req.body;
+  const hash = bcrypt.hashSync(password, 10);
+  const user = await UserModel.findOne({ username });
 
   if (user) {
-    res.send('user already exists');
+    res.send('Ya existe un usuario con ese nombre');
   } else {
-    const user = helpers.insertData(db, coleccion, {
-      username: req.body.username,
+    const user = UserModel.create({
+      username,
       password: hash,
+      email,
     });
+
     const token = jwt.sign(
       {
-        username: req.body.username,
-        expire: Date.now() + 1000 * 60 * 60 * 24 * 1, // 1 day
+        username,
+        expire: Date.now() + 1000 * 60 * 60 * 24 * 7, // 7 day
       },
       process.env.JWT_SECRET
     );
-    res.json({ registration: 'ok', token: token, username: req.body.username });
+    res.json({ registration: 'ok', token: token, username });
   }
 });
 
