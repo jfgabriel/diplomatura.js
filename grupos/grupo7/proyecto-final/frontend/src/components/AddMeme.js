bf1371ea70f75507aaf5472403ee78d654f88005
Juan Calou <juan.calou@patagonian.it>
2020-09-12 21:20:18 -0300

    default cat forr addmeme

--- grupos/grupo7/proyecto-final/frontend/src/components/AddMeme.js
+++ grupos/grupo7/proyecto-final/frontend/src/components/AddMeme.js
@@ -11,7 +11,7 @@ function AddMeme(usuario) {
   const [memeGuardado, setMemeGuardado] = useState(false);
   const [mensaje, setMensaje] = useState("");
   const [titulo, setTitulo] = useState("");
-  const [categoria, setCategoria] = useState("");
+  const [categoria, setCategoria] = useState("Arte");
   const [images, setImages] = useState([]);
   const [imagen, setImagen] = useState(null);
   const [respuesta, setRespuesta] = useState("");
